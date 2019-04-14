bf289022bc0daa47eea31d5af07495e4b0d288cf
James Sandford <jamesmsandford@googlemail.com>
2019-04-14 21:47:38 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='brewblox-tilt',
-    version='0.1.5',
+    version='0.1.6',
     long_description=open('README.md').read(),
     url='https://github.com/j616/brewblox-tilt',
     author='James Sandford',
