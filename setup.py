cc593366006a3b77f741379651035049fa6d9f74
James Sandford <jamesmsandford@googlemail.com>
2019-04-14 20:09:34 +0100

    Fix docker repo name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='brewblox-tilt',
-    version='0.1.3',
+    version='0.1.4',
     long_description=open('README.md').read(),
     url='https://github.com/j616/brewblox-tilt',
     author='James Sandford',
