731b205175f16023a86d5143c0135cbe538d2f49
James Sandford <jamesmsandford@googlemail.com>
2019-05-18 18:31:00 +0100

    Don't use readme as python description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='brewblox-tilt',
     version='1.2.0',
-    long_description=open('README.md').read(),
+    long_description="Tilt hydrometer service for BrewBlox",
     url='https://github.com/j616/brewblox-tilt',
     author='James Sandford',
     author_email='brewblox-tilt@j616s.co.uk',
